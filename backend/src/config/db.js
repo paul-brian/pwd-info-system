a7b9d3cfc39e0d6cbdb53e207a85d496d739f30e
Paul Brian B. Duterte <dutertepaulbrian43@gmail.com>
2026-03-13 22:10:57 +0800

    fix start script for render and server.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,22 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "pwd_system"
+// });
+
+// module.exports = db;
+
+  const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "pwd_system"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = db;
